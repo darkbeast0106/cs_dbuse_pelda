66c4039cb9da64a34cfd43559c64cba5a67df5ef
Budaházi Bence <bence0630@gmail.com>
2019-01-13 17:19:13 +0100

    Kész program.

--- CS_MySQL_Konzol_DEMO/Program.cs
+++ CS_MySQL_Konzol_DEMO/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,38 @@ namespace CS_MySQL_Konzol_DEMO
     {
         static void Main(string[] args)
         {
+            try
+            {
+                string connstring = "Server=localhost;Database=cs_pelda_db;Uid=root;Password=;";
+                var conn = new MySqlConnection(connstring);
+                conn.Open();
+                string sql = "SELECT * FROM felhasznalok";
+
+                var command = conn.CreateCommand();
+                command.CommandText = sql;
+                //MySqlCommand command = new MySqlCommand(sql, conn); 
+                /*
+                 * Mindkét megoldás ugyan azt eredményezi.
+                 */
+
+                var reader = command.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id = (int)reader["id"];
+                    string user = (string)reader["user"];
+                    string pass = (string)reader["pwd"];
+
+                    Console.WriteLine("{0}. Felhasználónév: {1}  Jelszó: {2}", id, user, pass);
+                }
+                reader.Close();
+                conn.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Hiba: "+ex.Message);
+            }
+
+            Console.ReadLine();
         }
     }
 }
